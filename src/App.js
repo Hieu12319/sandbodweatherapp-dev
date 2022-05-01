c44e0d855da80fc3f801bb661c7945bd270171ba
Hieu Nguyen <htn619@gmail.com>
2022-04-30 17:43:51 -0700

    working app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./styles.css";
+import weatherData from "./weatherData"
+import WeatherForcast from "./Components/WeatherForcast";
+// import WeatherIcon from "./Components/WeatherIcon";
 
 export default function App() {
+  const allWeatherData = weatherData.map((ele, index) => {
+    return <WeatherForcast {...ele} key={index} />
+  });
+  console.log(weatherData)
   return (
     <div className="App">
-      <h1>My WeatherIcons App</h1> 
+      <section>
+        {allWeatherData}
+        </section>
+     
     </div>
   );
 }
---
+++ src/Components/WeatherForcast.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import WeatherData from "./WeatherData";
+
+export default function WeatherForcast (props) {
+    return (
+        <div className="weather">
+            <WeatherIcon  img={props.img}/>
+            <WeatherData conditions={props.conditions} time={props.time}/>
+        </div>
+    );
+}
